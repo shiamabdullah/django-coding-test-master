7a4075210f8bcea5fefdfa43fd18b8f8438f2222
shiam <shiamabdullah1@gmail.com>
2021-12-31 01:33:50 +0600

    fixed the list

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -30,6 +30,7 @@ class ProductListView(ListView):
 
         if filter_val!="":
             pro=Product.objects.filter(Q(title__icontains=filter_val) & Q(created_at__contains=date_filter_val))
+            
         else:
             pro=Product.objects.all()
         return pro
@@ -42,15 +43,20 @@ class ProductListView(ListView):
         
         context['title_filter']= self.request.GET.get('title_filter','')
         context['date_filter']= self.request.GET.get('date_filter','')
-        context["all_table_fields"]=Product._meta.get_fields()        
+        # context["all_table_fields"]=Product._meta.get_fields()        
 
+        variant_filter= self.request.GET.get('variant_filter','')
+        
         variant_types = Variant.objects.values()
-        product_variant = ProductVariant.objects.values().order_by('variant_id')
-        product_variant_price = ProductVariantPrice.objects.values()
+        print(variant_filter)
+        product_variant = ProductVariant.objects.all()
+        
+        
+        product_variant_price = ProductVariantPrice.objects.all()
 
-        context['product_variant'] = list(product_variant.all())
+        context['product_variant'] = product_variant
         context['variant_types'] = list(variant_types.all())
-        context['product_variant_price'] = list(product_variant_price.all())
+        context['product_variant_price'] = product_variant_price
         # context['request'] = ''
         # context['request2'] = ''
         
