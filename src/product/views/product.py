56b0b713f592621e52cce8736d22960994f7e0b5
shiam <shiamabdullah1@gmail.com>
2021-12-31 00:03:47 +0600

    filtering product done with pagination fix

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -4,6 +4,8 @@ from product.models import Product, Variant
 from product.models import ProductVariant, ProductVariantPrice
 from django.views.generic import ListView
 from product.filter import ProductFilter
+from django.db.models import Q
+
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
 
@@ -17,32 +19,44 @@ class CreateProductView(generic.TemplateView):
 
 class ProductListView(ListView):
     template_name = "products/list.html"
-    model=Product
+    model= Product
     paginate_by= 5
     context_object_name = 'products'
     
     def get_queryset(self):
-        filter_string = {}
-        print(self.request.GET)
-        for key in self.request.GET:
-            if self.request.GET.get(key):
-                filter_string[key] = self.request.GET.get(key)
-        return Product.objects.filter(**filter_string)
+        filter_val=self.request.GET.get("title_filter","")
+        date_filter_val=self.request.GET.get("date_filter","")
+        print(filter_val,date_filter_val)
+
+        if filter_val!="":
+            pro=Product.objects.filter(Q(title__icontains=filter_val) & Q(created_at__contains=date_filter_val))
+        else:
+            pro=Product.objects.all()
+        return pro
+   
+ 
     
     def get_context_data(self, **kwargs):
+        
         context = super(ProductListView, self).get_context_data(**kwargs)
         
+        context['title_filter']= self.request.GET.get('title_filter','')
+        context['date_filter']= self.request.GET.get('date_filter','')
+        context["all_table_fields"]=Product._meta.get_fields()        
+
         variant_types = Variant.objects.values()
         product_variant = ProductVariant.objects.values().order_by('variant_id')
         product_variant_price = ProductVariantPrice.objects.values()
-        context['product_filter']= ProductFilter(self.request.GET,queryset=self.get_queryset())
-        
+
         context['product_variant'] = list(product_variant.all())
         context['variant_types'] = list(variant_types.all())
         context['product_variant_price'] = list(product_variant_price.all())
-        context['request'] = ''
+        # context['request'] = ''
+        # context['request2'] = ''
         
-        if self.request.GET:
-            context['request'] = self.request.GET['title__icontains']
         return context
+        # if self.request.GET:
+        #     context['request'] = self.request.GET['title__icontains']
+        #     context['request2'] = self.request.GET['created_at']
+        # return context
 
